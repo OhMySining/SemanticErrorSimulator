1af28c89c7e52170b693a39045553e52b9c1702c
Sining <darrelcenir004@gmail.com>
2024-10-12 17:12:52 +0800

    Add files via upload

---
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string YesNo;
+
+            Console.WriteLine("\nThis program accepts 0 and perform the first 20 of Fibonacci Sequence.");
+
+            do 
+            {
+            int loop = 0, num1, num2 = 1, temp; //the variables are inside the "do" so that when it loops, the variables will restart.
+
+            Console.Write("\nEnter 0: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+                if (num1 == 0)
+                {
+                    while (loop < 20)
+                    {
+                        Console.WriteLine(num1); 
+                        temp = num1; 
+                        num1 = num2; 
+                        num2 = temp + num2;
+                        loop++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid Input!");
+                }
+
+            Console.Write("\nRestart? (Yes/No): ");
+            YesNo = Console.ReadLine();
+
+            }
+            while (YesNo == "Yes");
+
+            Console.WriteLine("\nThank you for using my Finonacci Sequence Project!");
+
+            Console.ReadKey(true);
+        }
+    }
+}
