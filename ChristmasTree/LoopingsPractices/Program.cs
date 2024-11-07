ff5a9dcdd61a93bfde61a5a36e88cfdfe173ab11
Sining <darrelcenir004@gmail.com>
2024-11-07 23:17:05 +0800

    Add files via upload

---
+++ ChristmasTree/LoopingsPractices/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+                {
+                    for (int y = 20; y > i; y--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int x = 0; x <= i; x++)
+                    {
+                        Console.Write("X ");
+                    }
+                    Console.WriteLine();
+                }
+            for (int z = 0; z <= 5; z++)
+                {
+                    for (int x = 0; x < 15; x++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int w = 0; w < 6; w++)
+                    {
+                        Console.Write("I ");
+                    }
+                    Console.WriteLine();
+                }
+            Console.ReadKey(true);
+        }
+    }
+}
