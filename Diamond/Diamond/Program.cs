060802048adbc00a648eadad59295fe1fd271b01
Sining <darrelcenir004@gmail.com>
2024-11-09 15:45:11 +0800

    Add files via upload

---
+++ Diamond/Diamond/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Diamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 20; y > x; y--)
+                {
+                    Console.Write(" ");
+                }
+                for (int z = 0; z <= x; z++)
+                {
+                    Console.Write("X ");
+                }
+                Console.WriteLine();
+            }
+            for (int v = 20; v >= 0; v--)
+            {
+                for (int w = 20; w > v; w--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= v; j++)
+                {
+                    Console.Write("X ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
