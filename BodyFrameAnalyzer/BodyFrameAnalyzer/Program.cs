156d641ffb7233a9cc03b1a41eaeeed39a6562c8
Sining <darrelcenir004@gmail.com>
2024-11-16 12:45:47 +0800

    Add files via upload
    NOTE: These are not the actual figures in real life. I just created these to be used in
    my Activity)

---
+++ BodyFrameAnalyzer/BodyFrameAnalyzer/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace BodyFrameAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int weight, height;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.SetCursorPosition(50, 1);
+            Console.WriteLine("BODY FRAME ANALYZER");
+
+            Console.SetCursorPosition(25, 3);
+            Console.Write("Enter your Name: ");
+            name = Console.ReadLine();
+
+            Console.SetCursorPosition(25, 4);
+            Console.Write("Enter your weight (lbs): ");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.SetCursorPosition(25, 5);
+            Console.Write("Enter your height (inch): ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Console.SetCursorPosition(25, 7);
+            Console.Write($"Greetings, {name}. ");
+
+            switch (height)
+            {
+                case 58:
+                    if (weight <= 111)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 121)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 131)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 59:
+                    if (weight <= 113)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 123)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 134)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 60:
+                    if (weight <= 115)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 126)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 137)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 61:
+                    if (weight <= 118)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 129)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 140)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 62:
+                    if (weight <= 121)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 132)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 143)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 63:
+                    if (weight <= 124)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 135)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 147)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 64:
+                    if (weight <= 127)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 138)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 155)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+                case 65:
+                    if (weight <= 130)
+                    {
+                        Console.Write("Your body is a Small Frame.");
+                    }
+                    else if (weight <= 141)
+                    {
+                        Console.Write("Your body is a Medium Frame.");
+                    }
+                    else if (weight <= 155)
+                    {
+                        Console.Write("Your body is a Large Frame.");
+                    }
+                    break;
+            }
+        Console.ReadKey(true);
+        }
+    }
+}
---
+++ BodyFrameAnalyzer/BodyFrameAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BodyFrameAnalyzer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BodyFrameAnalyzer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a2b5ed5-a1ad-4422-8f45-9fa583a0a628")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
